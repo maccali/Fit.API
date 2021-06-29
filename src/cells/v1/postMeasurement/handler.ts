eeecf4bb457733c61f6174713c74bca3136a57b1
unknown <guilherme.maccali@universo.univates.br>
2021-06-29 15:48:05 -0300

    fix: ajusts on project

--- src/cells/v1/postMeasurement/handler.ts
+++ src/cells/v1/postMeasurement/handler.ts
@@ -34,27 +34,69 @@ module.exports.post = async (event: any) => {
   console.log('endTime', endTime)
 
   const minutes = (endTime - startTime) / (1000 * 60)
-  const bpm = measurements.length / minutes
+  // const bpm = measurements.length / minutes
+  const numberOfBpmMeasurements = measurements.length
+  let sumBpms = 0
   const seconds = (endTime - startTime) / ((1000 * 60) / 60)
   const hours = (endTime - startTime) / (((1000 * 60) / 60) * 60 * 60)
   console.log('minutes', minutes)
 
+  let mapingTime = []
+
+  measurements.forEach((item) => {
+    sumBpms = sumBpms + item.bpm
+    const dateItem = new Date(item.time)
+    const mapYear = dateItem.getFullYear()
+    const mapMonth = dateItem.getMonth() + 1
+    const mapDay = dateItem.getDay()
+    const mapHours = dateItem.getHours()
+    const mapMinutes = dateItem.getMinutes()
+    const mapIdentify = `${mapYear}-${mapMonth}-${mapDay}T${mapHours}:${mapMinutes}`
+
+    if (mapingTime[mapIdentify]) {
+      mapingTime[mapIdentify].push(item)
+    } else {
+      mapingTime[mapIdentify] = []
+      mapingTime[mapIdentify].push(item)
+    }
+  })
+
+  const beatsPerMinute = []
+  const bpm = sumBpms / numberOfBpmMeasurements
+
+  const beatMinutes = Object.keys(mapingTime)
+
+  beatMinutes.forEach((item) => {
+    const numberOfMeasures = mapingTime[item].length
+    let sumBpmInMinute = 0
+    mapingTime[item].forEach((element) => {
+      sumBpmInMinute = sumBpmInMinute + element.bpm
+    })
+    beatsPerMinute.push({ identifier: item, bpm: sumBpmInMinute / numberOfMeasures })
+  })
+
+  const data = {
+    id: uuid(),
+    measureTime,
+    bpm,
+    measurementTimeMinutes: minutes,
+    measurementTimeSeconds: seconds,
+    measurementTimeHours: hours,
+    userId,
+    createdAt: new Date().toISOString(),
+    measurements,
+    beatsPerMinute
+  }
+
+  console.log('DATA =>', data)
+
   let putItemParams = {
     TableName: TABLE_NAME,
-    Item: {
-      id: uuid(),
-      measureTime,
-      bpm,
-      measurementTimeMinutes: minutes,
-      measurementTimeSeconds: seconds,
-      measurementTimeHours: hours,
-      userId,
-      createdAt: new Date().toISOString(),
-      measurements
-    }
+    Item: data
     // ReturnValues: 'ALL_OLD'
   }
 
+  console.log('putItemParams =>', putItemParams)
   try {
     await dynamodb.put(putItemParams).promise()
     return {
